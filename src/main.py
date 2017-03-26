726cb6291f1d296ac453c7eabe317c473b0a3fac
Karen Galstyan <karen.galstyan.ib@gmail.com>
2017-03-26 20:59:08 +0400

    Added Limit Handler, separated grabber logic from main

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import twittergrabber as grabber
+import sys
+
+def main():
+	QUERY_PROFILE=str(sys.argv[1])
+	print("Querying "+QUERY_PROFILE)
+	api=grabber.Authenticate()
+	tweets=grabber.QueryProfile(api,QUERY_PROFILE,2)
+	print(str(tweets))
+main()
--- src/twittergrabber.py
+++ src/twittergrabber.py
@@ -1,27 +1,34 @@
 import sys
 import tweepy
-import csv
 import json
 import time
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 
-
 CONSUMER_KEY = "YqAo5ZRvG1QpPkcGG6rJ0JqwU"
 CONSUMER_SECRET = "AiM11Y5qhg6zsnbrMepUqxe7ejWuwm6NYdck2nOLlzXYcR6yJC"
 ACCESS_TOKEN="781620753456308224-3pGPA8366kQLCwsnpm46clwaebrszWV"
 ACCESS_SECRET="fBNJ3ayrwgq3xkPJuvOKrEL1gLwg71jF1bD8H7beXuwnN"
-START_TIME=time.time()
 
-auth = OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+"""Handling Twitter API request Limits"""
+def limit_handler(cursor):
+	while True:
+		try:
+			yield cursor.next()
+		except tweepy.RateLimitError:
+			time.sleep(15 * 60)
+def Authenticate():
+	auth = OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+	auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+	#TODO fix JSON parser error 
+	api = tweepy.API(auth)#,parser=tweepy.parsers.JSONParser())
+	return api
 
-api = tweepy.API(auth,parser=tweepy.parsers.JSONParser())
-
-user = api.user_timeline('Reuters')
-#print(user)
-
-results = api.search(q="Reuters", count=100)
-print (results)
+def QueryProfile(api,QUERY_PROFILE,limit):
+	user = api.user_timeline(QUERY_PROFILE)
+	tweets=[]
+	for tweet in limit_handler(tweepy.Cursor(api.user_timeline,id=QUERY_PROFILE).pages(limit)):
+		tweets.append(tweet)
+	return tweets
 
